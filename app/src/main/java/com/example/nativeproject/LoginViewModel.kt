f6299d57a7c0fa3fed88a27e661849403f154113
Arttu Koskenniemi <t9koar01@students.oamk.fi>
2022-03-19 21:57:16 +0200

    Firebase auth added

--- app/src/main/java/com/example/nativeproject/LoginViewModel.kt
+++ app/src/main/java/com/example/nativeproject/LoginViewModel.kt
@@ -1,18 +1,30 @@
 package com.example.nativeproject
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginViewModel : ViewModel() {
     var user = mutableStateOf("")
     var loggedIn = mutableStateOf(false)
 
     fun login(username: String, pw: String) {
-        user.value = username
-        loggedIn.value = true
+        Firebase.auth
+            .signInWithEmailAndPassword(username, pw)
+            .addOnSuccessListener {
+                user.value = username
+                loggedIn.value = true
+            }
+            .addOnFailureListener() {
+                Log.d("*************", "error occurred")
+                Log.d("XXXXXXXXXXXXXXX", it.message!!)
+            }
     }
 
     fun logout() {
+        Firebase.auth.signOut()
         user.value = ""
         loggedIn.value = false
     }
